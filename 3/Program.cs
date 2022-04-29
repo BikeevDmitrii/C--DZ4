88312df0b04c7485494b3f189a3636c4547cb8d5
«Dmitrii <bimas2006@mail.ru>
2022-04-30 00:42:10 +0300

    4 домашняя работа v3

---
+++ 3/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Напишите программу, которая задаёт массив из некоторого количества элементов и
+// выводит их на экран с помощью функций. (можно спрашивать количество вводимых элементов и вводить все значения с клавиатуры)
+
+Console.Write("Введите колличество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] Mac()
+{
+    int[] a = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return a;
+}
+void Print(int[] a)
+{
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write($"{a[i]} ");
+    }
+}
+int[] array = Mac();
+Print(array);
